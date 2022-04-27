fc84b7788adabe7cf490de6e8222f71cc8b34711
Martin Berg Alstad <600878@stud.hvl.no>
2022-04-27 20:33:46 +0200

    gh-pages config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: `martials-website`,
-    siteUrl: `https://www.yourdomain.tld`,
-    pathPrefix: "/martials-website",
+    siteUrl: `https://h600878.github.io/`,
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+      "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+  ]
 };
