2a2d3626c4776dd5facb2aca356186e6714f80a7
Martin Berg Alstad <600878@stud.hvl.no>
2022-05-11 15:47:34 +0200

    Added picture of yours truly on the frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import * as React from "react"
 import Layout from "../components/Layout";
+import {StaticImage} from "gatsby-plugin-image";
+
+const pictureStyle = {
+    maxWidth: "300px",
+    border: "solid grey",
+    borderRadius: "10px",
+}
 
 const IndexPage = () => {
     return (
@@ -8,6 +15,9 @@ const IndexPage = () => {
             headline={"Velkommen!"}
             children={
                 <div>
+                    <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
+                        <StaticImage style={pictureStyle} src={"../images/me.jpg"} alt={"Et bilde av meg på fjelltur"}/>
+                    </div>
                     <p>Hei, jeg heter Martin og studerer for tiden Informasjonsteknologi på Høgskulen på Vestlandet i
                         Bergen.</p>
                 </div>
