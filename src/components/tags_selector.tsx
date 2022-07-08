1ca482f533d2bf87985a37999d40edac974cc6d9
Martin Berg Alstad <600878@stud.hvl.no>
2022-07-08 22:10:27 +0200

    Added scroll by drag when using a mouse

--- src/components/tags_selector.tsx
+++ src/components/tags_selector.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Tag from "./tag";
 import { useTranslation } from "gatsby-plugin-react-i18next";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 interface TagsSelector {
     allTag?: string,
@@ -52,7 +53,7 @@ const TagsSelector = ({ allTag = "All", selectedTag, tagMap, onClick, id, classN
         function isOverflowingHorizontally(element: HTMLElement | null): boolean {
             if (element !== null) {
                 let sum = 0;
-                const children = element.children;
+                const children = element.children[0].children;
 
                 if (hideTags) {
                     sum -= 90; // Makes room for the hide button
@@ -71,43 +72,54 @@ const TagsSelector = ({ allTag = "All", selectedTag, tagMap, onClick, id, classN
         }
     }, [selectedTag]);
 
+    const scrollContainer = React.useRef<HTMLElement>(null);
+
+    React.useEffect(() => {
+        if (scrollContainer.current) {
+            scrollContainer.current.scrollTo(0, 0);
+        }
+    }, []);
+
     return (
-        <div id={ id } className={ `flex gap-1 ${ hideTags ? `overflow-scroll pb-3 ${ isOverflowing ? "" : "" }`
-            : "flex-wrap mb-2" } ${ className }` }>
-            <>
-                {
-                    allTag !== undefined ?
-                        <Tag
-                            name={ allTag }
-                            onClick={ onClick !== undefined ? () => onClick(allTag) : undefined }
-                            className={ `hover:border-primaryPurple ${ selectedTag === allTag ? "!border-primaryPurple" : "" }` }/>
-                        : null
-                }
-                {
-                    tagMap?.map((tag: any) =>
-                        <div key={ tag.key }>
-                            <Tag name={ tag.key }
-                                 value={ tag.value }
-                                 onClick={ onClick !== undefined ? () => onClick(tag.key) : undefined }
-                                 className={ `hover:border-primaryPurple w-max
+        <div id={ id }>
+            <ScrollContainer // TODO add visual, to show it's possible to drag
+                innerRef={ scrollContainer }
+                vertical={ false }
+                className={ `flex gap-1 mb-2 ${ hideTags ? `overflow-scroll` : "flex-wrap" } ${ className }` }>
+                <>
+                    {
+                        allTag !== undefined ?
+                            <Tag
+                                name={ allTag }
+                                onClick={ onClick !== undefined ? () => onClick(allTag) : undefined }
+                                className={ `hover:border-primaryPurple ${ selectedTag === allTag ? "!border-primaryPurple" : "" }` }/>
+                            : null
+                    }
+                    {
+                        tagMap?.map((tag: any) =>
+                            <div key={ tag.key }>
+                                <Tag name={ tag.key }
+                                     value={ tag.value }
+                                     onClick={ onClick !== undefined ? () => onClick(tag.key) : undefined }
+                                     className={ `hover:border-primaryPurple w-max
                                      ${ selectedTag === tag.key ? "!border-primaryPurple" : "" }` }/>
-                        </div>)
-                }
-                { /*TODO scroll on PC by dragging the mouse*/ }
-                {
-                    isOverflowing ?
-                        <>
-                            <div className={ `text-transparent min-w-max mx-2 ${ !hideTags ? "hidden" : "" }` }>
-                                { hideTags ? t("showMore") : null }
-                            </div>
-                            <Tag name={ hideTagsText.toString() } onClick={ toggleTags }
-                                 hoverTitle={ hideTags ? t("showMoreTags") : t("showLessTags") }
-                                 className={ `hover:border-primaryPurple min-w-max ${ hideTags ?
-                                     "absolute bg-white dark:bg-gray-900 right-0" : "" } shadow-sm shadow-primaryPurple` }/>
-                        </>
-                        : null
-                }
-            </>
+                            </div>)
+                    }
+                    {
+                        isOverflowing ?
+                            <>
+                                <div className={ `text-transparent min-w-max mx-2 ${ !hideTags ? "hidden" : "" }` }>
+                                    { hideTags ? t("showMore") : null }
+                                </div>
+                                <Tag name={ hideTagsText.toString() } onClick={ toggleTags }
+                                     hoverTitle={ hideTags ? t("showMoreTags") : t("showLessTags") }
+                                     className={ `hover:border-primaryPurple min-w-max ${ hideTags ?
+                                         "absolute bg-white dark:bg-gray-900 right-0" : "" } shadow-sm shadow-primaryPurple` }/>
+                            </>
+                            : null
+                    }
+                </>
+            </ScrollContainer>
         </div>
     )
 };
