4c6ccd0bebef9c7fb8a7c666403b4f5ccf6fe2c7
Martin Berg Alstad <600878@stud.hvl.no>
2022-07-30 19:31:39 +0200

    Started making matrix

--- src/components/truth-table.tsx
+++ src/components/truth-table.tsx
@@ -25,11 +25,31 @@ const TruthTable = ({ expression, className, id }: TruthTable) => {
         }
     }
 
+    /*
+        TODO count nr of atomic values
+        2 atomic    3 atomic
+        T T         T T T
+        T F         T T F
+        F T         T F T
+        F F         T F F
+                    F T T
+                    F T F
+                    F F T
+                    F F F
+     */
+
+    // Matrix of length 8 x 3
+    const list = new Array(3);
+    for (let i = 0; i < list.length; i++) {
+        list[i] = new Array(Math.pow(2, list.length));
+    }
+
     return (
-        <div className={ `border border-gray-500 grid grid-flow-col w-fit ${ className }` } id={ id }>
+        <div className={ `border border-gray-500 rounded-lg overflow-auto grid grid-flow-col w-fit ${ className }` }
+             id={ id }>
             {
-                expressions.map(exp => (
-                    <div key={ exp.toString() /*TODO better key*/ }>
+                expressions.map((exp, index) => (
+                    <div key={ index }>
                         <span className={ "w-fit px-2 border border-gray-500" }>{ exp.toString() }</span>
                     </div>
                 ))
