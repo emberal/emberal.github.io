9ceb551a3121b41ef31fffbab6e34b4f930d818b
Martin Berg Alstad <600878@stud.hvl.no>
2022-08-03 00:00:37 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fb45658014496d301244.js"
+    "url": "webpack-runtime-b175d8f37cbae3ee5d67.js"
   },
   {
     "url": "framework-20beb081f8592fdc4016.js"
   },
   {
-    "url": "styles.69b5f1977df0234fccc1.css"
+    "url": "styles.85010a185c1c117c172d.css"
   },
   {
     "url": "app-174111e7fd7f0ea642ca.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4011ff853985bad410d605da5d8dcfd9"
+    "revision": "a3e35c8faf7b43cf4be70333bacd0212"
   },
   {
     "url": "polyfill-ce8719fa795835be8823.js"
